2a3f301095bbebc97d0d30bf97969ceaa9a74f8f
Dave <wenqiweiabcd@gmail.com>
2016-10-13 19:59:37 +1100

    updated schema, updated dump file, recipe maker

---
+++ database/recipemker/mkres.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <assert.h>
+#include <libpq-fe.h>
+#include <string.h>
+#include <fstream>
+#include <limits>
+
+#define errprint(...) {fprintf (stderr, __VA_ARGS__);}
+
+int chomp (std::string & twat){ 
+    int wspace_begin;
+    for (wspace_begin = 0; wspace_begin< twat.length(); ++wspace_begin){
+        if (!isspace(twat[wspace_begin])){
+            break;
+        }
+    }
+    if (wspace_begin == twat.length()) return -1;
+    int wspace_end;
+    for (wspace_end = twat.length() - 1; wspace_end >= 0; --wspace_end){
+        if (!isspace  (twat [wspace_end])){
+            break;
+        }
+    }
+    
+    twat = twat.substr (wspace_begin, wspace_end + 1);    
+    return 0;
+}
+
+PGconn* dbcon (const char* comminfo){
+    PGconn* conn = PQconnectdb (comminfo);  
+    return (PQstatus(conn) == CONNECTION_OK)?conn:NULL;
+}
+
+PGresult* dbexec (PGconn *conn, std::string cmd){  
+    PGresult   *res;
+    res = PQexec (conn,cmd.c_str());
+    if (PQresultStatus(res) != PGRES_COMMAND_OK && PQresultStatus(res) != PGRES_TUPLES_OK){
+            errprint( "command failed: %s\n"
+                    "while attempting to execute: %s\n", 
+                    PQerrorMessage(conn),cmd.c_str());
+            //exit (1);
+            return NULL;
+    }
+    return res;
+}
+
+int main (int argc, char** argv){
+    if (argc != 2){
+        errprint ("look, I came here for an argument!\n");
+        exit (1);
+    }
+    
+	char stackbufarr[200];
+	char* stackbuf = stackbufarr;
+	char opcode [5];
+	opcode [5] = 0;
+    PGconn* conn;
+    FILE* ofile = fopen (argv[1], "w");
+        
+    if (!(conn = dbcon("dbname = MacroTrack"))){
+        errprint ("error connecting to db");
+        exit (1);
+    }
+	int consumed;
+    std::string recname;
+    printf ("enter the name of the recipe\n");
+    std::cin >> recname;
+    chomp (recname);
+    std::string ingr_query_base
+        ("select * from ingredients where name ~ ");
+    printf ("enter a fresh recipe ID\n");
+    int rid;
+    std::cin >> rid;
+    PGresult* rs;
+    const char* rec_fmt = "insert into recipes (\"id\",\"name\", \"instruction_file\") values (%d, \'%s\', \'%s\');\n";
+    const char* cont_fmt = "insert into contains (\"recipe\", \"ingredient\", \"amount\") values (%d,%d,%lf);\n"; 
+    std::string ifname = recname + ".instr";
+    fprintf (ofile, rec_fmt, rid, recname.c_str(), (recname + ".instr").c_str()); 
+   // assert (rs = dbexec (conn, "BEGIN"));
+    for (;;){
+        printf ("enter ingredient regex\n");
+        std::string ingredient_regex;
+        std::cin >> ingredient_regex;
+        if (chomp (ingredient_regex)  == -1){
+            printf ("invalid ingredient regex\n");
+            continue;
+        }
+        //std::cout << ingredient_regex;
+        if (ingredient_regex == "nomore"){
+            break;
+        }
+        int len = ingredient_regex.length();
+        std::string ingr_query = ingr_query_base + '\'' + ingredient_regex + '\'';
+        assert (rs = dbexec (conn, ingr_query));
+        if (!PQntuples(rs)){
+            printf ("no match for regex %s\n", ingredient_regex.c_str());
+            continue;
+        }
+        printf ("select the correct ingredient\n");
+        int i = 0;
+        for (i = 0; i < PQntuples(rs); ++i){
+           printf ("%d: ",i);
+           printf ("%s\n", PQgetvalue(rs, i, 1)); 
+        }
+        int sel = 0;
+        for (;;){
+            if (!(std::cin >> sel)){
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                errprint ("invalid selection, enter a number between %d and %d\n",0,i - 1);
+            }else if (sel < 0 || sel >= i){ 
+                errprint ("invalid selection, enter a number between %d and %d\n",0,i - 1);
+            }else break;
+        }
+        int igr_id = atoi (PQgetvalue (rs, sel, 0)); 
+        //printf ("%d\n", igr_id);
+        printf ("enter the amount of the ingredient in grammes\n");
+        double grammes;
+        std::cin >> grammes;
+        grammes /= (double)100;
+        fprintf (ofile, cont_fmt, rid, igr_id, grammes); 
+   }
+}
--- database/schema.sql
+++ database/schema.sql
@@ -9,13 +9,13 @@ create table Users (
 create table Ingredients (
        	id serial not null,
         name text not null,
-        calories float
+        calories float not null
                 constraint has_caloties check (calories >= 0),
-        sugar float
+        sugar float not null
                 constraint has_sugar check (sugar >= 0),
-        protein float
+        protein float not null
                 constraint has_protein check (sugar >= 0),
-        fat float
+        fat float not null
                 constraint has_fat check (sugar >= 0),
         primary key (id)              
 );
@@ -31,24 +31,24 @@ create table Recipes (
 
 create table Contains (
 	id serial not null,
-        recipe smallint
+        recipe smallint not null
                 constraint valid_recipe references Recipes (id),
-        ingredient smallint 
+        ingredient smallint  not null
                 constraint valid_ingredient references Ingredients (id),
-        amount float
+        amount float not null
                 constraint sane_amoint check (amount >= 0),
-		unit text not null,
+		
         primary key (id),
         unique (recipe, ingredient)
 );
 
 create table Ratings (
 	id serial not null,
-        userid smallint
+        userid smallint not null
                 constraint valid_user references Users(id),
-        recipe smallint
+        recipe smallint not null
                 constraint valid_recipe references Recipes (id),
-        rating smallint
+        rating smallint not null
                 constraint sane_rating check (rating >= 0 and rating <= 10),
         primary key (id),
         unique (userid, recipe)
