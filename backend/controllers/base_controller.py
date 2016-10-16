0da430333faf97f106ca8ead134492994d50ea1d
Fraser Hemphill <fraser.hemphill@yahoo.com.au>
2016-10-17 02:14:26 +1100

    Logout option, more locked down user base views, create a new recipe (needs UX work), rating HTML

--- backend/controllers/base_controller.py
+++ backend/controllers/base_controller.py
@@ -10,19 +10,31 @@ class BaseController(MethodView):
   def requires_auth(f):
     @functools.wraps(f)
     def decorated(*args, **kwargs):
+
+      potential_user_id = BaseController().is_logged_in()
       
-      auth = request.cookies.get("auth_token", "")
-      email = request.cookies.get("email", "")
-      potential_user_id = User().check_auth(email, auth)
-      
-      if not auth or not potential_user_id:
+      if not potential_user_id:
         return redirect("/login", code=302)
 
       request.user_id = potential_user_id
       return f(*args, **kwargs)
 
     return decorated
-	
+
+  def get_user_id(self):
+    from flask import request
+    email = request.cookies.get("email", "")
+
+    return User().id_from_email(email)
+
+  def is_logged_in(self):
+    from flask import request
+    auth = request.cookies.get("auth_token", "")
+    email = request.cookies.get("email", "")
+    potential_user_id = User().check_auth(email, auth)
+
+    return potential_user_id
+
   def get():
     return "404 not found."
 
